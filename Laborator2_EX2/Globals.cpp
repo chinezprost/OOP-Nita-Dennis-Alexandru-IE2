4828f4d31b4e33ba2c03f7520ea4961d6b275eb2
chinezprost <playschan@gmail.com>
2023-02-27 11:28:57 +0200

    added global.cpp globals.h

---
+++ Laborator2_EX2/Globals.cpp
@@ -0,0 +1,60 @@
+#pragma once
+#include "Student.h"
+#include "Globals.h"
+#include <cstring>
+
+int CompareStudentMath(Student first, Student second)
+{
+	float _value1 = first.GetGradeMath();
+	float _value2 = second.GetGradeMath();
+	if (_value1 > _value2)
+		return 1;
+	if (_value1 < _value2)
+		return -1;
+	return 0;
+}
+
+int CompareStudentHistory(Student first, Student second)
+{
+	float _value1 = first.GetGradeHistory();
+	float _value2 = second.GetGradeHistory();
+	if (_value1 > _value2)
+		return 1;
+	if (_value1 < _value2)
+		return -1;
+	return 0;
+}
+
+int CompareStudentEnglish(Student first, Student second)
+{
+	float _value1 = first.GetGradeEnglish();
+	float _value2 = second.GetGradeEnglish();
+	if (_value1 > _value2)
+		return 1;
+	if (_value1 < _value2)
+		return -1;
+	return 0;
+}
+
+int CompareStudentAverageGrade(Student first, Student second)
+{
+	float _value1 = first.GetAverageGrade();
+	float _value2 = second.GetAverageGrade();
+	if (_value1 > _value2)
+		return 1;
+	if (_value1 < _value2)
+		return -1;
+	return 0;
+}
+
+int CompareStudentName(Student first, Student second)
+{
+	char* _value1 = first.GetName();
+	char* _value2 = second.GetName();
+
+	if (strcmp(_value1, _value2) > 0)
+		return 1;
+	if (strcmp(_value1, _value2) < 0)
+		return -1;
+	return 0;
+}
--- Laborator2_EX2/Globals.h
+++ Laborator2_EX2/Globals.h
@@ -2,58 +2,8 @@
 #include "Student.h"
 #include <cstring>
 
-int CompareStudentMath(Student first, Student second)
-{
-	float _value1 = first.GetGradeMath();
-	float _value2 = second.GetGradeMath();
-	if (_value1 > _value2)
-		return 1;
-	if (_value1 < _value2)
-		return -1;
-	return 0;
-}
-
-int CompareStudentHistory(Student first, Student second)
-{
-	float _value1 = first.GetGradeHistory();
-	float _value2 = second.GetGradeHistory();
-	if (_value1 > _value2)
-		return 1;
-	if (_value1 < _value2)
-		return -1;
-	return 0;
-}
-
-int CompareStudentEnglish(Student first, Student second)
-{
-	float _value1 = first.GetGradeEnglish();
-	float _value2 = second.GetGradeEnglish();
-	if (_value1 > _value2)
-		return 1;
-	if (_value1 < _value2)
-		return -1;
-	return 0;
-}
-
-int CompareStudentAverageGrade(Student first, Student second)
-{
-	float _value1 = first.GetAverageGrade();
-	float _value2 = second.GetAverageGrade();
-	if (_value1 > _value2)
-		return 1;
-	if (_value1 < _value2)
-		return -1;
-	return 0;
-}
-
-int CompareStudentName(Student first, Student second)
-{
-	char* _value1 = first.GetName();
-	char* _value2 = second.GetName();
-
-	if (strcmp(_value1, _value2) > 0)
-		return 1;
-	if (strcmp(_value1, _value2) < 0)
-		return -1;
-	return 0;
-}
+int CompareStudentMath(Student first, Student second);
+int CompareStudentHistory(Student first, Student second);
+int CompareStudentEnglish(Student first, Student second);
+int CompareStudentAverageGrade(Student first, Student second);
+int CompareStudentName(Student first, Student second);
